fda1178b035f16f5e9a929228039d443082f6981
BraNtswembuu <obakeng.boikanyo270@gmail.com>
2025-06-24 17:52:08 +0200

    Add Lesson 2 exercises

---
+++ lesson 2.py
@@ -0,0 +1,50 @@
+name = "BraNtswembu"
+message = 'Hello, there!'
+# print(name)
+# print(message)
+
+# Different ways to create strings 
+first_name = "Tom"
+last_name = "Ford"
+city = 'Bloemfontein'
+greetings = "Welcome to Python Programming!"
+# print(first_name)
+# print(last_name)
+# print(city)
+# print(greetings)    
+
+#  When to use which quotes
+sentence = 'She said, "Python is great"!'
+# print(sentence)
+
+contraction = "Don't worry, it's easy!"
+# print(contraction)
+
+# Multi-line strings
+story = """Once upon a time,
+there was a brave King who ruled a vast kingdom.
+He loved his people and fought for their rights.
+"""
+# print(story)
+
+word = "Python"
+print(len(word))  # Length of the string
+
+new_message = "Hello, World!"
+# print(len(new_message))  # Length of the new message
+
+# Accessing characters in a string
+print(word[0])  # First character
+print(word[-1])  # Last character
+print(word[1])  # Second character
+print(word[-2])  # Second last character
+
+# Escape characters
+
+# New line
+# print("First line\nSecond line")
+
+# Tab
+# print("Name:\tTom\nAge:\t25")
+
+# print('She said, "Hello!"')
